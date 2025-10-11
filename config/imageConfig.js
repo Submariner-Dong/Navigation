32038078bf1862ef451410c31c466cb2f5c2a4bf
Submariner <2629025002@qq.com>
2025-10-11 19:46:18 +0800

    更新内容：
    优化院内导航逻辑，基础项目单独处理（图片和导航路径）
    
    问题：
    
    室内导航图片显示不全，放射科多一步，儿科少一步

---
+++ config/imageConfig.js
@@ -0,0 +1,13 @@
+// 图片地址配置
+const imageConfig = {
+  GATE: "/public/images/gate.jpg",
+  PASSAGE: "/public/images/passage.jpg",
+  "2-RADIOLOGY-1": "/public/images/2-radiology-1.jpg",
+  "2-PEDIATRIC-1": "/public/images/2-pediatric-1.jpg",
+  "1-STAIRS": "/public/images/1-stairs.jpg",
+  "1-ELEVATOR": "/public/images/1-elevator.jpg",
+  "2-ELEVATOR": "/public/images/2-elevator.jpg",
+  "1-ORTHODONTICS-1": "/public/images/1-orthodontics-1.jpg"
+};
+
+export default imageConfig;
--- pages/navigation/navigation.js
+++ pages/navigation/navigation.js
@@ -1,45 +1,11 @@
+import navigationConfig from "../../config/navigationConfig";
+
 Page({
   data: {
     navigationArray: [
-      "正畸科", "放射科", "儿科", "儿童口腔科"
+      "正畸科", "放射科", "儿科/儿童口腔科"
     ],
-    navigationInfo: { 
-      "正畸科": {
-        steps: [
-          { image: "/public/images/gate.jpg", text: "进入同济大学口腔医院" },
-          { 
-            options: [
-              { type: "stairs", image: "/public/images/1-stairs.jpg", text: "沿楼梯上楼至2楼" },
-              { type: "elevator", image: "/public/images/1-elevator.jpg", text: "上电梯前往2楼" }
-            ]
-          },
-          { image: "/public/images/1-orthodontics-1.jpg", text: "成功到达正畸科，治疗顺利！" }
-        ]
-      },
-      "放射科": {
-        steps: [
-          { image: "/public/images/gate.jpg", text: "进入同济大学口腔医院" },
-          { image: "/public/images/passage.jpg", text: "穿过过道" },
-          { image: "/public/images/2-radiology-1.jpg", text: "成功到达放射科，治疗顺利！" }
-        ]
-      },
-      "儿科": {
-        steps: [
-          { image: "/public/images/gate.jpg", text: "进入同济大学口腔医院" },
-          { image: "/public/images/passage.jpg", text: "穿过过道" },
-          { image: "/public/images/2-pediatric-1.jpg", text: "乘电梯或走楼梯前往4楼" },
-          { image: "/public/images/2-radiology-1.jpg", text: "成功到达儿童口腔科，治疗顺利！" }
-        ]
-      },
-      "儿童口腔科": {
-        steps: [
-          { image: "/public/images/gate.jpg", text: "进入同济大学口腔医院" },
-          { image: "/public/images/passage.jpg", text: "穿过过道" },
-          { image: "/public/images/2-pediatric-1.jpg", text: "乘电梯或走楼梯前往4楼" },
-          { image: "/public/images/2-radiology-1.jpg", text: "成功到达儿童口腔科，治疗顺利！" }
-        ]
-      }
-    },
+    navigationInfo: navigationConfig,
 
     destination: "",
     value: [0],
